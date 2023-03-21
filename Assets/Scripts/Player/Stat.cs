0117ec20469fbe6f0bdfdbf77c6e67814e19a25a
ashibar <56782952+ashibar@users.noreply.github.com>
2023-03-21 20:17:58 +0900

    Player stat base added

---
+++ Assets/Scripts/Player/Stat.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat
+{
+    private float hp;
+    private float speed;
+    
+    public float Hp { get => hp; set => hp = value; }
+    public float Speed { get => speed; set => speed = value; }
+}
