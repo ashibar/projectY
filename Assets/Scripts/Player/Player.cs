44546ed36c00ab9093fff023611b07c54a5682f5
ashibar <56782952+ashibar@users.noreply.github.com>
2023-03-21 20:31:03 +0900

    230321_01

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 스크립트 이름 : Player
+/// 담당자 : 이용욱
+/// 요약 : 플레이어 오브젝트 최상위 스크립트, stat정보 보유
+/// 비고 :
+/// </summary>
+
 public class Player : MonoBehaviour
 {
     public Stat_so stat;
+    // stat 세부 정보는 Stat스크립트 참조해주세요
+    // 더 필요한 스탯이 생기면 바로 바로 피드백 부탁드립니다.
 
     protected virtual void Start()
     {
