2629b94f1f927a82c83c6e93b4e720c98a58afd7
pkJs1124 <127395920+pkJs1124@users.noreply.github.com>
2023-03-24 19:34:38 +0900

    3241934

--- Assets/Scripts/Magic/Magic/ShotManage.cs
+++ Assets/Scripts/Magic/Magic/ShotManage.cs
@@ -1,53 +1,66 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System.Threading.Tasks;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
+/// <summary>
+/// 작성자 : 박종성
+/// 마지막 업데이트 시기 : 23/3/24 / 19:30
+/// 업데이트 내용 : 쿨타임 적용
+/// GameObject = ShotManage // Player
+/// </summary>
 
 public class ShotManage : MonoBehaviour
 {
     //public GameObject[] preFabSpell;
-    Camera cam;
-    [SerializeField]
-    private Spell_Stat_so stat_so;
-    [SerializeField]
-    private Spell_Stat stat;
-
     public Vector2 dir_toMouse;
-
-    [SerializeField]
-    protected bool iscooltime = false;
     //================================================
-    [SerializeField]
-    protected bool isFireSpell = true;
-    [SerializeField]
-    protected bool isIceSpell = false;
-    [SerializeField]
-    protected bool isEarthSpell = false;
-    [SerializeField]
-    protected bool isSpell = false;
+    [SerializeField] private Spell_Stat_so stat_so;
+    [SerializeField] private Spell_Stat stat;
+    [SerializeField] private GameObject[] Spells;
     //================================================
-    [SerializeField]
-    protected bool isMobAttacked = false;
-    [SerializeField]
-    protected bool[] SkillCode;
-    [SerializeField]
-    float Spell_speed = 5f;
-    [SerializeField]
-    private GameObject[] Spells;
+    [SerializeField] protected float cooltime = 1;
+    [SerializeField] float Spell_speed = 5f;
+    //================================================
+    [SerializeField] protected bool isFireSpell = true;
+    [SerializeField] protected bool isIceSpell = false;
+    [SerializeField] protected bool isEarthSpell = false;
+    //================================================
 
-    private float start_Time;
-    private float time;
-    private float direction = 2;
-    private bool isShooted = false;
+    [SerializeField] protected bool isChecked = true;
+    [SerializeField] protected bool isUseSpell = false;
+    //================================================
+    
+    
+
+    
     protected void Start() { 
-        cam = Camera.main;
+        if(!isChecked || isUseSpell) // 이렇게 안해주면 작동안함!!!!!!
+        {
+            isChecked = true;
+            isUseSpell = false;
+        }
     }
     
     public void Update()
     {
-        Shoot();
+        if (isUseSpell)
+        {
+            StartCoroutine(ResetSkillCoroutine(cooltime));
+        }
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            if(isChecked)
+            {
+                Shoot();
+                isUseSpell = true;
+                isChecked = false;
+            }
+                
+            
+        }
+
     }
     
     protected int DoingSpell() //몇번째 스펠인지 정해준다.
@@ -59,34 +72,36 @@ public class ShotManage : MonoBehaviour
         return SpellNumbers;
     }
     
-
+   
     public virtual void Shoot()
     {
-        if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log(Spells + " " + DoingSpell());
-            //Vector2 len = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
-            //Vector2 dir = new Vector2(len.x, len.y).normalized;
+            Vector2 len = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+            Vector2 dir = new Vector2(len.x, len.y).normalized;
             
             GameObject Spell = Instantiate(Spells[DoingSpell()], transform.position, Quaternion.identity);
-
-            Spell.GetComponent<Rigidbody2D>().velocity = dir_toMouse * Spell_speed;
-            isShooted = true;
-            End_Shoot(Spell); // 제작중
+            
+            Debug.Log(dir_toMouse);
+            Spell.GetComponent<Rigidbody2D>().velocity = dir * Spell_speed; //dir_toMouse
+            
         }
-        
     }
 
-    public virtual void End_Shoot(GameObject Spell)
+    
+    IEnumerator ResetSkillCoroutine(float coltimes) //스킬 쿨타임 
     {
-        if (isShooted) //제작중
+        const float baseTime = 1f; // BaseTime이 최소단위
+        isUseSpell = false; //들어가자마자 자기 자신의 조건을 비활성화 // 안그러면 Update에서 무한하게 실행됨
+        while (coltimes > 0) //쿨타임 메인 로직, coltimes를 받아서 baseTime초만큼씩 줄임
         {
-            time += Time.deltaTime;
-            if (time > direction)
-            {
-                
-                time = 0;
-            }
+            coltimes -= baseTime;
+            yield return new WaitForSeconds(baseTime);
         }
+        isChecked = true; //Shoot 활성화 로직
+        
+        yield break;
     }
+
+
+
 }
