b04993e8b79a3505cb3288521a038c41e6aa71d1
pkJs1124 <127395920+pkJs1124@users.noreply.github.com>
2023-03-30 13:43:50 +0900

    123

--- Assets/Scripts/Magic/MagicManager.cs
+++ Assets/Scripts/Magic/MagicManager.cs
@@ -14,12 +14,16 @@ public class MagicManager : MonoBehaviour
     private void Start()
     {
         player = GetComponentInParent<Player>();
+        //Mob = GetComponentInParent<Mob>();
+        //몹을 GetComponent로 가져온다.
+        //그 이후 몹이나 플레이어쪽에서 유닛에 접근한 특정 ID값 ) ex. UnitType을 판단해 Update문에서 변경
         shotManager.AddRange(GetComponentsInChildren<ShotManage>());
     }
 
     // Update is called once per frame
     private void Update()
-    {
+    {   
+ 
         foreach(ShotManage manager in shotManager)
         {
             manager.dir_toMouse = player.dir_toShoot;
