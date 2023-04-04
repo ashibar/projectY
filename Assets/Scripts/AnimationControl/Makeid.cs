30b0827c216e5be0d3358829e82fe3a2ebe90005
mana114 <127661531+mana114@users.noreply.github.com>
2023-04-04 18:15:15 +0900

    sdf

---
+++ Assets/Scripts/AnimationControl/Makeid.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Makeid : MonoBehaviour
+{
+    //고유값을 넣기위한 스크립트
+    public int id;
+    public bool isNpc;
+}
