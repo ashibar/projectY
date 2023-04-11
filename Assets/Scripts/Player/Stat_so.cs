970c2687cd98673274640b4a969fbc894ec0cc82
ashibar <56782952+ashibar@users.noreply.github.com>
2023-04-11 18:35:02 +0900

    230411_00

--- Assets/Scripts/Player/Stat_so.cs
+++ Assets/Scripts/Player/Stat_so.cs
@@ -14,6 +14,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "DefaultStat", menuName = "Scriptable Object/Stat", order = int.MaxValue)]
 public class Stat_so : ScriptableObject
 {
+    [SerializeField] private int id;
+    [SerializeField] private string name_unit;
+    
     [SerializeField] private float hp;
     [SerializeField] private float speed;
     [SerializeField] private float hp_regen;
@@ -27,16 +30,16 @@ public class Stat_so : ScriptableObject
     [SerializeField] private float cooldown;
     [SerializeField] private float amount;
 
-    public float Hp { get => hp; }
-    public float Speed { get => speed; }
-    public float Hp_regen { get => hp_regen; }
-    public float Armor { get => armor; }
-    public float Damage { get => damage; }
-    public float Speed_projectile { get => speed_projectile; }
-    public float Duration_projectile { get => duration_projectile; }
-    public float Range_projectile { get => range_projectile; }
-    public float Cooldown { get => cooldown; }
-    public float Amount { get => amount; }
-
-    
+    public int Id { get => id; set => id = value; }
+    public string Name_unit { get => name_unit; set => name_unit = value; }
+    public float Hp { get => hp; set => hp = value; }
+    public float Speed { get => speed; set => speed = value; }
+    public float Hp_regen { get => hp_regen; set => hp_regen = value; }
+    public float Armor { get => armor; set => armor = value; }
+    public float Damage { get => damage; set => damage = value; }
+    public float Speed_projectile { get => speed_projectile; set => speed_projectile = value; }
+    public float Duration_projectile { get => duration_projectile; set => duration_projectile = value; }
+    public float Range_projectile { get => range_projectile; set => range_projectile = value; }
+    public float Cooldown { get => cooldown; set => cooldown = value; }
+    public float Amount { get => amount; set => amount = value; }
 }
