0fc82a661287c23cafea56a671dc4bf708c950e3
ashibar <56782952+ashibar@users.noreply.github.com>
2023-04-12 19:37:34 +0900

    aa

---
+++ Assets/Scripts/Magic/Parts/Parts.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parts : MonoBehaviour
+{
+    protected virtual void Start()
+    {
+
+    }
+
+    protected virtual void Update()
+    {
+
+    }
+
+    public virtual void Applier(GameObject proj, Stat_Spell stat)
+    {
+
+    }
+}
