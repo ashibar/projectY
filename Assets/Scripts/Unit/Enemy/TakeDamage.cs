0eced89d0222c590511a4075e17144cd765bd8f9
ashibar <leeyoung2004@naver.com>
2023-06-01 16:49:34 +0900

    230601_1649

--- Assets/Scripts/Unit/Enemy/TakeDamage.cs
+++ Assets/Scripts/Unit/Enemy/TakeDamage.cs
@@ -7,17 +7,21 @@ public class TakeDamage : MonoBehaviour
     [SerializeField]
     public GameObject hudDamageText;
     public Transform hudpos;
+    private bool isCooltime;
     private void Update()
     {
-        Damagesend(10);
+        if (!isCooltime)
+            StartCoroutine(Damagesend(10));
     }
 
-    public void Damagesend(int damage)
+    public IEnumerator Damagesend(int damage)
     {
+        isCooltime = true;
         GameObject hudText = Instantiate(hudDamageText);//텍스트
         hudText.transform.position = hudpos.position;//텍스트 포지션
         hudText.GetComponent<DamageText>().damage = damage;
         Debug.Log(damage);
-        
+        yield return new WaitForSeconds(3);
+        isCooltime = false;
     }
 }
